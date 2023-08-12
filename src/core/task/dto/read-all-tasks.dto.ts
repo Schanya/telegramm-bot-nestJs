ec8ecb6a5170da7c9b6775bea4dff40f0fc4801f
Schanya <anyashemelevadm@gmail.com>
2023-08-12 17:03:26 +0300

    add task module

---
+++ src/core/task/dto/read-all-tasks.dto.ts
@@ -0,0 +1,3 @@
+export class ReadAllTaskDto {
+  name?: string;
+}
---
+++ src/core/task/task.model.ts
@@ -0,0 +1,37 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../user/user.model';
+
+export interface TaskCreationAttrs {
+  title: string;
+  description: string;
+  time: Date;
+}
+
+@Table({ tableName: 'tasks', paranoid: true })
+export class Task extends Model<Task, TaskCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  title: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  description: string;
+
+  @Column({ type: DataType.DATE, allowNull: false })
+  time: Date;
+
+  @BelongsTo(() => User, 'user_id')
+  user: User;
+}
