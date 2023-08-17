2b0b1e7dd1dffea2f5ba6d308b7c69a783f63df7
Schanya <anyashemelevadm@gmail.com>
2023-08-18 01:28:59 +0300

    edit formatDateToString method

--- src/core/telegramm/scenes/utils/date-methods.ts
+++ src/core/telegramm/scenes/utils/date-methods.ts
@@ -30,7 +30,7 @@ export function compareDateWithCurrent(date: Date) {
 }
 
 export function formatDateToString(date: Date): string {
-  const day = String(date.getDay()).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
   return `${day}.${month}.${year}`;
