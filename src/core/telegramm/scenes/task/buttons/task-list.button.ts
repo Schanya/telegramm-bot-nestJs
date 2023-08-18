86278368d7de8f64301894ad0356cc53709882d6
Schanya <anyashemelevadm@gmail.com>
2023-08-18 12:35:24 +0300

    edit task list buttons

--- src/core/telegramm/scenes/task/buttons/task-list.button.ts
+++ src/core/telegramm/scenes/task/buttons/task-list.button.ts
@@ -1,15 +1,22 @@
 import { Markup } from 'telegraf';
 
 import { Task } from '@task/task.model';
+import { railwayServiceTimeZoneOffset } from '@telegramm/enums';
 
 export function tasksListButtons(tasks: Task[]) {
   const buttons = [];
+  const currentDate = new Date();
 
-  tasks.forEach((task) =>
-    buttons.push(Markup.button.callback(task.title, task.id.toString())),
-  );
+  tasks.forEach((task) => {
+    const difference = task.time.getTime() - currentDate.getTime();
+    const notificationTime = `${difference >= 0 ? '' : ' 🔚'}`;
+
+    buttons.push(
+      Markup.button.callback(task.title + notificationTime, task.id.toString()),
+    );
+  });
 
   return Markup.inlineKeyboard(buttons, {
-    columns: 5,
+    columns: 1,
   });
 }
